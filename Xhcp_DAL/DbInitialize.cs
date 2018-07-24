1911aa1856574f5c46626f260e7cc69cfc34b1af
xmwang <wxm1026sx@163.com>
2018-07-25 01:13:59 +0800

    test

---
+++ Xhcp_DAL/DbInitialize.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Xhcp_DAL
+{
+    public static class DbInitialize
+    {
+        //public static void Initialize(DataContext context) {
+
+        //}
+    }
+}
