82e7fa1109ca66832f0ad3fa5696552553d2618b
wangxiaoming <wxm1026sx@163.com>
2018-07-25 14:36:18 +0800

    test

---
+++ Xhcp_DAL/DesignTimeDBContextFactory.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Xhcp_DAL
+{
+    public class DesignTimeDbContextFactory : IDesignTimeDbContextFactory<ProductContext>
+    {
+        public ProductContext CreateDbContext(string[] args)
+        {
+            var builder = new DbContextOptionsBuilder<ProductContext>();
+            builder.UseMySql("UserID = root; Password = xmwang1.q; Host = 47.104.139.46; Port=3306; Database = xhcp_Db");
+            return new ProductContext(builder.Options);
+        }
+    }
+}
---
+++ Xhcp_DAL/Migrations/20180725062302_init.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Xhcp_DAL.Migrations
+{
+    public partial class init : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Courses",
+                columns: table => new
+                {
+                    Id = table.Column<long>(nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    RowVersion = table.Column<DateTime>(nullable: false),
+                    CreteTime = table.Column<DateTime>(nullable: false),
+                    Name = table.Column<string>(maxLength: 20, nullable: false),
+                    Description = table.Column<string>(maxLength: 500, nullable: false),
+                    Category = table.Column<int>(nullable: false),
+                    Price = table.Column<decimal>(nullable: false),
+                    Discount = table.Column<decimal>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Courses", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Courses");
+        }
+    }
+}
