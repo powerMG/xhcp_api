9d4ac812ee5ee301a40c2de5a839b2b142ea56da
wangxiaoming <wxm1026sx@163.com>
2018-07-31 21:05:55 +0800

    test

---
+++ API_XhcpProject/Controllers/AccountController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API_XhcpProject.Controllers
+{
+    [Produces("application/json")]
+    [Route("api/Account")]
+    public class AccountController : Controller
+    {
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "test" };
+        }
+    }
+}
